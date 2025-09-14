bd72243f08a60dcb992e6f03ce99dd7af0eb97c7
Icemic <bingfeng.web@gmail.com>
2025-09-14 09:06:29 +0800

    feat: add transition effect for background

--- src/actors/background.tsx
+++ src/actors/background.tsx
@@ -1,12 +1,25 @@
+import { animated, useTransition } from '@momoyu-ink/kit';
 import { useSnapshot } from 'valtio';
 import { gameState } from '../state/game';
 
 export function BackgroundActor() {
   const backgroundState = useSnapshot(gameState.background);
 
-  if (!backgroundState.src) {
-    return null;
-  }
+  const transitions = useTransition(backgroundState.src ? [backgroundState.src] : [], {
+    keys: (src) => src,
+    from: { opacity: 0 },
+    enter: { opacity: 1 },
+    leave: { opacity: 0 },
+    config: {
+      duration: backgroundState.fadeTime,
+    },
+  });
 
-  return <sprite label="背景图" src={backgroundState.src} />;
+  return (
+    <container label="背景容器">
+      {transitions((style, src) => (
+        <animated.sprite label="背景图" src={src} opacity={style.opacity} />
+      ))}
+    </container>
+  );
 }
